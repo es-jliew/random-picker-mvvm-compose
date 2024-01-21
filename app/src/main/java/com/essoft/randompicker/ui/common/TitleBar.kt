cc622f62b876345fe8b76d3ff84b2833fc35f25b
es-jliew <es.jliew@gmail.com>
2024-01-21 17:38:53 +0800

    - Remove unused imports.

--- app/src/main/java/com/essoft/randompicker/ui/common/TitleBar.kt
+++ app/src/main/java/com/essoft/randompicker/ui/common/TitleBar.kt
@@ -3,13 +3,8 @@ package com.essoft.randompicker.ui.common
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Restaurant
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -18,7 +13,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.essoft.randompicker.R
-import com.essoft.randompicker.constants.ColorConstants
 
 @Composable
 fun TitleBar() {
